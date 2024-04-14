cd2a60e60ec00833e576b9e4f58e587e3416dc29
Hazal <113294096+hazalkntr@users.noreply.github.com>
2024-04-14 12:56:01 +0300

    added comments for explanations in motion_blurred_image_generator.py

--- motion_blurred_image_generator.py
+++ motion_blurred_image_generator.py
@@ -1,3 +1,8 @@
+# This is a Python program to generate an image that has been affected by motion blur, 
+# simulating a photograph taken with a mobile camera. 
+# Motion blur is a common artifact in images captured by moving cameras or 
+# moving objects within the scene during exposure.
+
 import cv2
 import numpy as np
 
